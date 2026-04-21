c451e11a3a00dc93ca069a22376a0ef3c6ddb109
Hemanth <hemanthh115533@gmail.com>
2026-04-21 13:38:16 +0530

    Update EDA using SQL.sql

--- EDA using SQL.sql
+++ EDA using SQL.sql
@@ -1,11 +1,17 @@
+--We are just going to explore the data and find trends, patterns or anything interesting like outliers!
+
+-- Looking at the percentage to see how big these layoffs were
 SELECT MAX(total_laid_off), MAX(percentage_laid_off)
 FROM layoffs_staging2;
 
+-- Checking companies that had percentage_laid_off as 1, which is basically 100 percent of their company laid off and ordered by funds_raised_millions.
 SELECT *
 FROM layoffs_staging2
 WHERE  percentage_laid_off = 1
 ORDER BY funds_raised_millions DESC;
 
+
+-- Companies with the most Total Layoffs
 SELECT company, SUM(total_laid_off)
 FROM layoffs_staging2
 GROUP BY company
@@ -14,11 +20,13 @@ ORDER BY 2 DESC;
 SELECT MIN(`date`), MAX(`date`)    -- time frame of the layoffs
 FROM layoffs_staging2;
 
+-- Industries with the most Total Layoffs
 SELECT industry, SUM(total_laid_off)
 FROM layoffs_staging2
 GROUP BY industry
 ORDER BY 2 DESC;
 
+-- this is the total in the past 3 years or in the dataset
 SELECT country, SUM(total_laid_off)
 FROM layoffs_staging2
 GROUP BY country
@@ -34,6 +42,8 @@ FROM layoffs_staging2
 GROUP BY stage
 ORDER BY 1 DESC;
 
+
+-- Companies with layoffs each month and doing a rolling total to fetch the collective total for the end of each year!
 WITH Rolling_Total AS
 (
 SELECT SUBSTRING(`date`,1,7) AS `MONTH`, SUM(total_laid_off) AS total_off
@@ -44,11 +54,13 @@ GROUP BY `MONTH` ORDER BY 1 ASC
 SELECT `MONTH`,total_off, SUM(total_off) OVER(ORDER BY `MONTH`) AS rolling_total
 FROM  Rolling_Total;
 
+-- Checking the top 5 companies with the most layoffs each year.
 SELECT company, YEAR(`date`), SUM(total_laid_off)
 FROM layoffs_staging2
 GROUP BY company, YEAR(`date`)
 ORDER BY 3 DESC;
 
+-- created the same as a CTE (company_year) and created another CTE (Company_Year_Rank) from the 1st CTE (company_year)
 WITH company_year (company, years, total_laid_off) AS
 (SELECT company, YEAR(`date`), SUM(total_laid_off)
 FROM layoffs_staging2
